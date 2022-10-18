c504620e65725188d992b53b8a34014162f98a3b
Enrique León <56865084+KIMEX117@users.noreply.github.com>
2022-10-18 10:00:54 -0600

    Unidad 5 - Trabajo en clase - Martes 18/Oct/22
    Actividad #6:  Implementa el controlador UserController y manda al menos 4 rutas distintas.

--- unidad5/reservaciones/routes/web.php
+++ unidad5/reservaciones/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
+use App\Http\Controllers\UserController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,6 +20,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+/*
 Route::get('saludo', function () {
     echo "Hola";
 });
@@ -29,6 +32,7 @@ Route::get('saludo/{name}', function ($name) {
 Route::get('saludo/{num1}/{num2}/{num3?}', function ($num1, $num2, $num3=0) {
     echo $num1 + $num2 + $num3;
 })->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
+*/
 
 //->where( ['num1', '[0-9]+'], ['num2', '[0-9]+'] );
 //->where('num1', '[0-9]+')->where('num2', '[0-9]+');
@@ -36,3 +40,9 @@ Route::get('saludo/{num1}/{num2}/{num3?}', function ($num1, $num2, $num3=0) {
 /*Route::post('suma/', function(Request $request) {
 
 });*/
+
+//TRABAJO EN CLASE 18/Octubre - Mes #10/2022
+Route::get('users/', [UserController::class, 'index']);
+Route::get('users/create', [UserController::class, 'create']);
+Route::get('users/{id}', [UserController::class, 'show']);
+Route::post('users/', [UserController::class, 'store']);
